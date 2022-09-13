5db27c6c921d8f93d9bcebc622a12926932483be
Kévin Hamon <kevin@hamon.app>
2022-09-13 13:27:38 +0200

    init repo

--- back/back/wsgi.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
-
-application = get_wsgi_application()
--- front/src/app/general/components/table/table-action.ts
+++
@@ -1,9 +0,0 @@
-import {Directive, EventEmitter, Input, Output} from "@angular/core";
-
-@Directive({selector: 'table-action'})
-export class TableAction {
-  @Input() label!: string;
-  // @Input() click!: Function;
-
-  @Output() click: EventEmitter<any> = new EventEmitter<any>();
-}
